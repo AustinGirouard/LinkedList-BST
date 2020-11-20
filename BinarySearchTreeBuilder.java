5b23a7024046ee4f39ef1129adde40ddb1af0acc
AustinGirouard <57466429+AustinGirouard@users.noreply.github.com>
2020-11-19 20:08:28 -0600

    Update BinarySearchTreeBuilder.java
    Added constructor,
    Implemented contains(String keyword),
    Implemented insert helper metod

--- BinarySearchTreeBuilder.java
+++ BinarySearchTreeBuilder.java
@@ -1,4 +1,42 @@
 public class BinarySearchTreeBuilder 
 {
+    BSTNode root;
+
+    public BinarySearchTreeBuilder()
+    {
+        this.root = null;
+    }
+
+    public boolean contains(String keyword)
+    {
+        BSTNode current = root;
+        while(current != null)
+        {
+            if(current.keyword.compareTo(keyword) < 0)
+                current = current.left;
+
+            else if(current.keyword.compareTo(keyword) > 0)
+                current = current.right;
+            
+            else
+                return true;
+        }
+        return false;
+    }
+
+
+    void insert(String keyword, FileData fd)
+    {
+        Record record = new Record(fd.id, fd.title, fd.author, null);
+        if(!this.contains(keyword))
+        {
+            insert(keyword, record);
+        }
+    }
+
+    boolean insert(String keyword, Record record)
+    {
+        return false;
+    }
     
 }
